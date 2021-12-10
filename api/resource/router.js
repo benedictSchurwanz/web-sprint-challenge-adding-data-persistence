600f8143ced28b443afff9c18605a59e636384eb
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-10 16:57:37 -0600

    add resources router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,27 @@
-// build your `/api/resources` router here
+const express = require('express')
+const Resources = require('./model')
+
+const resourcesRouter = express.Router()
+
+// get all Projects
+resourcesRouter.get('/', async (req, res, next) => {
+	try {
+		const data = await Resources.getProjects()
+		res.status(200).json(data)
+	} catch (err) {
+		next(err);
+	}
+});
+
+// add new Project
+resourcesRouter.post('/', async (req, res, next) => {
+	const resourceData = req.body;
+	Resources.createProject(resourceData)
+		.then(newResource => {
+			res.status(201).json(newResource)
+		})
+		.catch(next)
+})
+
+module.exports = resourcesRouter;
+
