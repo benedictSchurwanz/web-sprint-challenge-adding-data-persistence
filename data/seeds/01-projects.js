8f1e5c2be38fc4a25a07885ca8374feeec8b95f7
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-10 14:41:26 -0600

    edit seed to try case without product_description

--- data/seeds/01-projects.js
+++ data/seeds/01-projects.js
@@ -1,14 +1,21 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   return knex('projects').truncate()
     .then(function () {
       return knex('projects').insert([
-        { project_name: 'excellent project', project_description: 'excellent description', 
-          project_completed: false } ,
-          { project_name: 'super project', project_description: 'super description', 
-          project_completed: false }, 
-          { project_name: 'done project', project_description: 'done description', 
-          project_completed: true }
+        {
+          project_name: 'excellent project', 
+          project_description: 'excellent description',
+          project_completed: false
+        },
+        { 
+          project_name: 'super project', 
+          project_completed: false },
+        {
+          project_name: 'done project', 
+          project_description: 'done description',
+          project_completed: true
+        }
       ]);
     });
 };
