daa30185d5d03ec5a7d1b91c86aaf9f24b673eed
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-10 15:08:03 -0600

    rename projects seeds file

--- data/seeds/01-projects-seeds.js
+++ data/seeds/01-projects-seeds.js
@@ -4,15 +4,16 @@ exports.seed = function (knex) {
     .then(function () {
       return knex('projects').insert([
         {
-          project_name: 'excellent project', 
+          project_name: 'excellent project',
           project_description: 'excellent description',
           project_completed: false
         },
-        { 
-          project_name: 'super project', 
-          project_completed: false },
         {
-          project_name: 'done project', 
+          project_name: 'super project',
+          project_completed: false
+        },
+        {
+          project_name: 'done project',
           project_description: 'done description',
           project_completed: true
         }
