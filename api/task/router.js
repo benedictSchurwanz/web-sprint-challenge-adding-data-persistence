12768983c1ab971b10b6cfb7243944dbb6ead168
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-10 17:14:28 -0600

    Task router

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,24 @@
-// build your `/api/tasks` router here
+const express = require('express')
+const Tasks = require('./model')
+
+const tasksRouter = express.Router()
+
+tasksRouter.get('/', async (req, res, next) => {
+	try {
+		const data = await Tasks.getTasks()
+		res.status(200).json(data)
+	} catch (err) {
+		next(err);
+	}
+});
+
+tasksRouter.post('/', async (req, res, next) => {
+	const taskData = req.body;
+	Tasks.createTask(taskData)
+		.then(newTask => {
+			res.status(201).json(newTask)
+		})
+		.catch(next)
+})
+
+module.exports = tasksRouter;
