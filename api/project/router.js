76dafa06806bfb30a4d237352ee7a9561405d4e4
Benedict Schurwanz <19333755+benedictify@users.noreply.github.com>
2021-12-10 15:55:58 -0600

    add projects router get all

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,18 @@
-// build your `/api/projects` router here
+const express = require('express')
+const Projects = require('./model')
+
+const projectsRouter = express.Router()
+
+// get all Projects
+projectsRouter.get('/', async (req, res, next) => {
+	try {
+		const data = await Projects.getProjects()
+		res.status(200).json(data)
+	} catch (err) {
+		next(err);
+	}
+});
+
+// add new Project
+
+module.exports = projectsRouter;
--- api/server.js
+++ api/server.js
@@ -1,23 +1,17 @@
 const express = require('express');
-
-// import router
-// const FooRouter = require('.../foo-router.js')
+const projectsRouter = require('./project/router')
 
 const server = express();
 
 server.use(express.json())
+server.use('/api/projects', projectsRouter)
 
-// route 
-// server.use('/api/foos', FooRouter)
-
-// Endpoints: (should be in other router files.)
-
-// server.use((err, req, res, next) => {
-// 	res.status(err.status || 500).json({ message: err.message})
-// })
-
-server.get('/', (req, res) => { 
+server.get('/', (req, res) => {
 	res.status(200).json({ api: "server is up" })
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+	res.status(500).json({ message: err.message })
 })
 
 module.exports = server;
